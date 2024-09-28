db805ff4616bbdbe00a955be67b358d30d6125d0
AJ <angeljayden@ciencias.unam.mx>
2024-09-27 20:33:20 -0600

    Hice los cambios propuestos por el ayudante

--- practica02/practica02.hs
+++ practica02/practica02.hs
@@ -5,32 +5,34 @@ longitud (x:xs) = 1 + longitud (xs)
 
 sumaLista :: Num a => [a] -> a
 sumaLista [] = 0
-sumaLista (x:xs) = x + sumaLista xs 
+sumaLista (x:xs) = x + sumaLista xs
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento (x:xs) y bool
-    | bool = y:(x:xs)
-    | otherwise = (x:xs)++[y]
+agregaElemento [a] y bool
+    | bool = y:[a]
+    | otherwise = [a]++[y]
 
 maximoLista :: (Num a, Ord a) => [a] -> a
-maximoLista [x] = x 
+maximoLista [x] = x
 maximoLista (x:xs)
     | x > maximoLista xs = x
     | otherwise = maximoLista xs
 
 indice :: [a] -> Int -> a
-indice [] _ = error "indice fuera de rango"
-indice (x:xs) 0 = x
-indice (x:xs) n = indice xs (n-1)
+indice [a] n
+    |  0>n || n>longitud [a]-1= error "indice fuera de rango"
+indice (x:xs) n
+    |   n==0=x
+    |otherwise=indice xs (n-1)
 
 divisores :: Int -> [Int]
-divisores n = [x | x <- [1..n], n `mod` x == 0]       
+divisores n = [x | x <- [1..n], n `mod` x == 0]
 
 conjunto :: Eq a => [a] -> [a]
 conjunto [] = []
-conjunto (x:xs) = x : conjunto[y | y <- xs, y/=x]
+conjunto (x:xs) = x : conjunto [y | y <- xs, y/=x]
 
 
 numerosPares :: [Int] -> [Int]
-numerosPares xs = [x | x <- xs, x `mod` 2 == 0] 
+numerosPares xs = [x | x <- xs, x `mod` 2 == 0]
 
